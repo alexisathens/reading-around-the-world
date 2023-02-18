40702f12f4dce5d4cffc885c412c1314bc83e650
alexisathens <lexisathens1@gmail.com>
2023-02-18 13:47:09 -0700

    Assume standard Wiki extension and grab

--- Create Map.R
+++ Create Map.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(magrittr)
 library(readxl)
-library(RSelenium)
+#library(RSelenium)
+library(RJSONIO) # for reading Wiki JSON files
 
 ### download exported library from Goodreads ---------
 # download data by going to My Books > on side bar under Tools, select Import and export > click Export Library
@@ -18,61 +19,209 @@ books %<>%
 
 ### set up RSelenium for web scraping ---------
 
-# close docker container from before (skip if first run)
-system('docker stop firefox')
-system('docker rm firefox')
-
-# set up docker container
-system('docker run --platform linux/amd64 --name firefox -v /dev/shm:/dev/shm -d -p 4567:4444 -p 5901:5900 selenium/standalone-firefox:latest')
-
-# check new container open
-system('docker ps -a')
-
-# open a virtual network 
-system('open vnc://127.0.0.1:5901')
-
-# specify the remote driver
-remDr <- remoteDriver(port = 4567L, browser = "firefox")
-
-# open up a firefox window in the VNC
-remDr$open()
-
-# navigate to Wikipedia
-remDr$navigate('https://www.wikipedia.org/')
+# # close docker container from before (skip if first run)
+# system('docker stop firefox')
+# system('docker rm firefox')
+# 
+# # set up docker container
+# system('docker run --platform linux/amd64 --name firefox -v /dev/shm:/dev/shm -d -p 4567:4444 -p 5901:5900 selenium/standalone-firefox:latest')
+# 
+# # check new container open
+# # system('docker ps -a')
+# 
+# # open a virtual network 
+# system('open vnc://127.0.0.1:5901')
+# 
+# # specify the remote driver
+# remDr <- remoteDriver(port = 4567L, browser = "firefox")
+# 
+# # open up a firefox window in the VNC
+# remDr$open()
 
 
 
 ### scrape Wikipedia for author's URL ---------
 
-# initialize column to store author URL
-books$url <- NULL
+# # initialize column to store author URL
+# books$url <- NA
+# 
+# # define buffer to not overload website
+# buffer_sec <- 1
+# 
+# ## loop through all books and get author's Wiki URL
+# for(i in 1:nrow(books)){
+#   
+#   # send message to console
+#   cat(paste0("**** Retrieving info for author ", i, " of ", nrow(books), " ****  \n")); flush.console()
+#   
+#   if(!is.na(books$url[i])){
+#     # if the author URL has already been found (would be the case in reading the same author multiple times), skip
+#     next
+#   }
+#   
+#   # navigate to Wikipedia
+#   remDr$navigate('https://www.wikipedia.org/')
+#   Sys.sleep(buffer_sec)
+#   
+#   # search for search box element
+#   search_box <- remDr$findElement(using = 'id', value = 'searchInput')
+#   Sys.sleep(buffer_sec)
+#   
+#   # enter author name into search bar and go
+#   search_box$sendKeysToElement(list(books$author[i], key = 'enter'))
+#   Sys.sleep(buffer_sec)
+#   
+#   # give time for page to load
+#   remDr$setTimeout(type = "page load", milliseconds = 10000)
+#   
+#   # check if a disambiguation page
+#   ambiguous <- NULL
+#   
+#   try(ambiguous <- remDr$findElement(using = "link text", value = "disambiguation"), silent = TRUE)
+#   
+#   if(!is.null(ambiguous)){
+#     # if it's an ambiguous page, skip to the next author and continue
+#     next
+#   }
+#   
+#   # get URL of author webpage
+#   author_url <- remDr$getCurrentUrl()[[1]]
+#   Sys.sleep(buffer_sec)
+#   
+#   # parse URL, just keeping author extension (last element)
+#   author_short <- tail(str_split(author_url, "/")[[1]], 1)
+#   
+#   # store URL in df for all author instances
+#   books$url[which(books$author[i] == books$author)] <- author_short
+#   
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# ## need error catching!
+# 
+# # Douglas Stuart - ambiguous
+# # Isaac Fitzgerald - no side bar
+# 
+# # checking if country in the list exists
+# suppressMessages({
+#   try(elem <- remDr$findElement(using = "link text", value = this_country_name), silent = TRUE)
+# })
+# 
+# if(is.null(elem)){ # if the country isn't on the WTO website, skip it
+#   
+#   next # next skips the current iteration of the loop (skips to the next c)
+#   # see: https://www.datamentor.io/r-programming/break-next/
+#   
+# }
+# 
+# 
+# # search for search box element
+# search_box <- remDr$findElement(using = 'id', value = 'searchInput')
+# 
+# # enter author name into search bar and go
+# search_box$sendKeysToElement(list(books$author[5], key = 'enter'))
+# 
+# # give time for page to load
+# remDr$setTimeout(type = "page load", milliseconds = 10000)
+# 
+# # get URL of author webpage
+# author_url <- remDr$getCurrentUrl()[[1]]
+# 
+# # parse URL, just keeping author extension (last element)
+# author_short <- tail(str_split(author_url, "/")[[1]], 1)
 
-# define buffer to not overload website
-buffer_sec <- 2
-Sys.sleep(buffer_sec)
 
 
-## need error catching!
+
+### assume simple Wikipedia extension
+
+# there's a standard format for each author's Wikipedia extension, which is their full name with spaces replaced by underscores
+books$basic_url <- str_replace_all(books$author, " ", "_")
+# assume this is the case. if it doesn't work, we'll find the author manually later
 
 
-# Douglas Stuart - ambiguous
-# Isaac Fitzgerald - no side bar
+## manual edits to author
 
 
-# search for search box element
-search_box <- remDr$findElement(using = 'id', value = 'searchInput')
+# also initialize birth_place column because this is what we want to figure out
+books$birth_place <- NA
 
-# enter author name into search bar and go
-search_box$sendKeysToElement(list(books$author[5], key = 'enter'))
+# note that there are a few different breaking points:
+# (1) no Wikipedia page, i = 22
+# (2) ambiguous Wiki page, i = 6
+# (3) Wiki page but with no side bar info, i = 1
+# (4) side bar but no birth_place
+# strategy is to skip all of these cases and fill in manually
 
-# give time for page to load
-remDr$setTimeout(type = "page load", milliseconds = 10000)
+head(books)
 
-# get URL of author webpage
-author_url <- remDr$getCurrentUrl()[[1]]
+for(i in 1:nrow(books)){
+  
+  json_url <- paste0("https://en.wikipedia.org//w/api.php?action=query&format=json&prop=revisions&titles=", 
+                     books$basic_url[i], "&formatversion=2&rvprop=content&rvslots=*")
+  
+  
+  # first check if this is a bad link
+  bad_link <- NULL
+  
+  try(info <- fromJSON(json_url), silent = TRUE)
+  
+  if(!is.null(bad_link)){
+    # if this is a bad link, skip to the next author and continue
+    next
+  }
+  
+  # get main content block (giant text mass)
+  mass <- info$query$pages[[1]]$revisions[[1]]$slots$main[["content"]]
+  
+  if(is.null(mass)){ # if bad link again
+    next
+  }
+  
+  # check for field called birth_place
+  birth_avail <- str_detect(mass, "birth_place")
+  
+  if(!birth_avail){
+    # if this field isn't available, skip to the next author and continue
+    next
+  }
+  
+  ## clean up birth_place if it is available
+  mass_split <- str_split(mass, "\\n\\|")[[1]] # split on new lines
+  
+  birth_place <- mass_split[which(str_detect(mass_split, "birth_place"))] # search for birth_place keyword
+  
+  # clean up a bit to just get string
+  birth_place <- str_remove(birth_place, "birth_place")
+  birth_place <- str_remove(birth_place, "=")
+  birth_place <- str_remove_all(birth_place, "\\[\\[")
+  birth_place <- str_remove_all(birth_place, "\\]\\]")
+  birth_place <- trimws(birth_place)
+  # good enough for now
+  
+  if(birth_place == ""){
+    # if birth_place field is present but empty, skip for author
+    next
+  }
+  
+  # finally, track birth place string for author
+  books$birth_place[i] <- birth_place
+  
+}
 
-# parse URL, just keeping author extension (last element)
-author_short <- tail(str_split(author_url, "/")[[1]], 1)
+
+mean(is.na(books$birth_place)) # only got 1/3..
+
+books %>% 
+  filter(is.na(birth_place)) %>% 
+  distinct(author)
+
+
 
 
 
